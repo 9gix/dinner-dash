9e64fa9bebf51728cad0ce38590f1bf98405db31
Eugene <yeo.eugene.oey@gmail.com>
2014-05-23 16:42:01 +0800

    User Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_many :orders
+  validates :email, uniqueness: true
+  validates :full_name, presence: true
+  validates :display_name, length: {minimum: 2, maximium: 32}
 end
