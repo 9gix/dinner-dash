bf03bf92c4c3e9c068f353b1943661409c9b19fb
Eugene <yeo.eugene.oey@gmail.com>
2014-05-23 17:25:41 +0800

    Item validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
   has_and_belongs_to_many :categories
   has_many :order_items
   has_many :orders, :through => :order_items
+
+  validates :title, :description, :price, :presence => true
+  validates :title, :uniqueness => true
+  validates :price, numericality: {greater_than: 0}
 end
